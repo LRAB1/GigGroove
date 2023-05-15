db0b8dcdef4dfbd8652da29fe0819eac73444f2a
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2023-05-15 13:41:21 +0200

    tracks now return to much candidates.

--- server/data/setlist.js
+++ server/data/setlist.js
@@ -1,5 +1,5 @@
 const artist = "The Rolling Stones";
 const tour = "Licks World Tour";
-const songs = ["Brown Sugar", "You Got Me Rocking", "It's Only Rock 'n' Roll (but I Like It)", "Don't Stop", "Angie", "You Can't Always Get What You Want", "Midnight Rambler", "Tumbling Dice", "The Nearness of You", "Before They Make Me Run", "Sympathy for the Devil"];
+const songs = ["Brown Sugar"];
 
 module.exports = { artist, tour, songs };
--- server/src/models/spotify.model.js
+++ server/src/models/spotify.model.js
@@ -1,35 +1,82 @@
-//TODO build integration with spotify API.
+// Import the necessary modules
 const SpotifyWebApi = require('spotify-web-api-node');
-const {spotifyClient, spotifySecret} = require('../../../keys');
-//TODO: create artist as variable.
-const artist = "6uejjWIOshliv2Ho0OJAQN?si=wcoeBJVIQeWqZLPChpFDmQ";
+const { spotifyClient, spotifySecret } = require('../../../keys');
+const { artist, songs } = require('../../data/setlist');
 
-
+// Create a new instance of the SpotifyWebApi object and connect to the API
 function connectToSpotifyApi(spotifyClient, spotifySecret) {
-  // create a new instance of the SpotifyWebApi object
   const spotifyApi = new SpotifyWebApi({
     clientId: spotifyClient,
-    clientSecret: spotifySecret
+    clientSecret: spotifySecret,
   });
 
-  // retrieve an access token using the client ID and client secret
-  spotifyApi.clientCredentialsGrant().then((data) => {
-    spotifyApi.setAccessToken(data.body['access_token']);
-    console.log('Connected to Spotify API') ;
-  }).catch((error) => {
-    console.log('Error connecting to Spotify API:', error);
-  });
-
-  return spotifyApi;
+  return spotifyApi.clientCredentialsGrant()
+    .then((data) => {
+      spotifyApi.setAccessToken(data.body.access_token);
+      console.log('Connected to Spotify API');
+      return spotifyApi;
+    })
+    .catch((error) => {
+      console.log('Error connecting to Spotify API:', error);
+      throw error;
+    });
 }
 
-const getArtist = connectToSpotifyApi(spotifyClient, spotifySecret);
+// Search for the artist ID using the artist name provided in setlist.js
+function searchArtist(spotifyApi, artistName) {
+  return spotifyApi.searchArtists(artistName)
+    .then((data) => {
+      if (data.body.artists.total > 0) {
+        return data.body.artists.items[0].id;
+      } else {
+        console.log(`Artist not found: ${artistName}`);
+        return null;
+      }
+    })
+    .catch((error) => {
+      console.log(`Error searching for artist: ${artistName}`, error);
+      throw error;
+    });
+}
 
-/*
-const getArtist = spotifyApi.Artist(artist)
-  .then(data => {
-    console.log('Retrieved artist', data.body);
+// Connect to the Spotify API and search for the artist ID
+function getArtistId() {
+  return connectToSpotifyApi(spotifyClient, spotifySecret)
+    .then((spotifyApi) => searchArtist(spotifyApi, artist))
+    .then((artistId) => {
+      console.log(`Found artist ID: ${artistId}`); //Debug routine
+      return artistId;
+    });
+}
+
+getArtistId();
+
+// Create a new instance of the SpotifyWebApi object and connect to the API
+const spotifyApi = new SpotifyWebApi({
+  clientId: spotifyClient,
+  clientSecret: spotifySecret
+});
+
+// Get an access token using the built-in authorization tool
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => {
+    console.log('Connected to Spotify API');
+    spotifyApi.setAccessToken(data.body['access_token']);
+
+    // Use the access token to search for tracks on Spotify
+    spotifyApi
+      .searchTracks(songs.join(' '))
+      .then((data) => {
+        console.log(`Found ${data.body.tracks.total} tracks`);
+        data.body.tracks.items.forEach((track) => {
+          console.log(`Track name: ${track.name}, Artist: ${track.artists[0].name}`);
+        });
+      })
+      .catch((error) => {
+        console.log('Error searching for tracks:', error);
+      });
   })
-  .catch(error => {
-    console.log('Error retrieving artist', error);
-  });*/
+  .catch((error) => {
+    console.log('Error connecting to Spotify API:', error);
+  });
