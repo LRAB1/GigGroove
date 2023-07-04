cf16054629a1e6da4225c213d9385042a74c9fd3
LRAB1 <lexbant@gmail.com>
2023-07-04 15:06:33 +0200

    async funct pushin json to array

--- server/src/controllers/setlistfm.controller.js
+++ server/src/controllers/setlistfm.controller.js
@@ -1,34 +1,50 @@
-var https = require('follow-redirects').https;
-var fs = require('fs');
+const https = require('follow-redirects').https;
+const fs = require('fs');
+const { setlistFmKey } = require('../../../keys');
 
-const {setlistFmKey} = require('../../../keys')
+const retrievedSetlist = [];
+const iD = '4bb7d3c2';
 
-var options = {
-  'method': 'GET',
-  'hostname': 'api.setlist.fm',
-  'path': '/rest/1.0/setlist/4bb7d3c2',
-  'headers': {
-    'accept': 'application/json',
+const options = {
+  method: 'GET',
+  hostname: 'api.setlist.fm',
+  path: `/rest/1.0/setlist/${iD}`,
+  headers: {
+    accept: 'application/json',
     'x-api-key': setlistFmKey
   },
-  'maxRedirects': 20
+  maxRedirects: 20
 };
 
-var req = https.request(options, function (res) {
-  var chunks = [];
+const getRequest = () => {
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, function (res) {
+      const chunks = [];
 
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
+      res.on('data', function (chunk) {
+        chunks.push(chunk);
+      });
+
+      res.on('end', function () {
+        const body = Buffer.concat(chunks);
+        resolve(body.toString());
+      });
+
+      res.on('error', function (error) {
+        reject(error);
+      });
+    });
+
+    req.end();
   });
+};
 
-  res.on("end", function (chunk) {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
-
-  res.on("error", function (error) {
+(async () => {
+  try {
+    const body = await getRequest();
+    retrievedSetlist.push(body);
+    console.log(retrievedSetlist[0]);
+  } catch (error) {
     console.error(error);
-  });
-});
-
-req.end();
+  }
+})();
