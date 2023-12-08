b75d1c8aa9e0124059bd4083872f63a46961c937
LRAB1 <lexbant@gmail.com>
2023-12-08 12:12:26 +0100

    stashing cus no fix

--- server/src/routers/cleanup.router.js
+++ server/src/routers/cleanup.router.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const setlistModel = require('../models/setlist.model');
+const { formatSetlist} = require('../models/setlist.model');
 
 router.post('/cleanup', async (req, res) => {
   console.log('Cleanup request fired off');
