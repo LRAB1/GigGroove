170f726c1cdcf2acaa32ad2405f0a89c0e13a747
LRAB1 <lexbant@gmail.com>
2023-12-11 12:08:48 +0100

    Route works!

--- server/src/routers/spotify.router.js
+++ server/src/routers/spotify.router.js
@@ -1,23 +1,18 @@
-// spotify.router.js
 const express = require('express');
 const router = express.Router();
-const setlistModel = require('../models/setlist.model');
+const spotifyController = require('../controllers/spotify.controller');
 
-// Make sure you have a valid callback function for the post route
-router.post('/spotify', async (req, res) => {
-  console.log('Spotify request fired off');
-
+router.post('/groovify', async (req, res) => {
   try {
-    // Assuming you have some data to format, you can pass it to the formatSetlist function
-    const formattedData = setlistModel.formatSetlist(req.body);
+    const playlistId = await spotifyController.createPlaylist();
+    console.log('Playlist ID:', playlistId);
+    await spotifyController.addSongsToPlaylist(playlistId);
 
-    // You can send back the formatted data as a response or perform other operations
-    // Respond with the formatted data
-    res.json(formattedData);
-    console.log(formattedData);
+    console.log('All tracks added to the playlist successfully!');
+    res.status(200).json({ message: 'Spotify integration successful' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'An error occurred during setlist cleanup' });
+    res.status(500).json({ error: 'Spotify integration failed. An error occurred.' });
   }
 });
 
