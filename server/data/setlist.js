52e79b12abfe66340a38163567e60acae3bcfd16
LRAB1 <lexbant@gmail.com>
2023-12-11 12:34:10 +0100

    it all works again.

---
+++ server/data/setlist.js
@@ -0,0 +1,33 @@
+const artist = "HAEVN";
+const venue = "Koninklijk Theater Carré";
+const tour = "Koninklijk Theater Carré";
+const songs = [
+  {
+    "setNumber": 1,
+    "songs": [
+      "Keep your promise",
+      "The Sea",
+      "Where the Heart Is",
+      "Giving",
+      "Hold On To My Love",
+      "Throw Me a Line",
+      "We Are",
+      "Holy Ground",
+      "Welcome The Wind",
+      "City Lights",
+      "Trade It for the Night",
+      "Where Did You Go",
+      "The Other Side of Sea",
+      "Fortitude",
+      "One Day",
+      "Finding Out More",
+      "Bright Lights",
+      "Love Is a Game",
+      "Keep your promise",
+      "Back in the Water",
+      "Kite in a Hurricane"
+    ]
+  }
+];
+
+module.exports = { artist, venue, tour, songs };
