05d70a1b69979e7bd8db15fae46aaed793862262
LRAB1 <lexbant@gmail.com>
2024-05-29 20:49:01 +0200

    Flavour text added to keys.example

--- keys.example.js
+++ keys.example.js
@@ -1,6 +1,8 @@
+//TODO: get the spotifyClient and Secret from the controller and not hardcode it.
+//for dev purposes this works but should be made into a self filling variable.
 const setlistFmKey = ''
-const spotifyClient= ''
-const spotifySecret = ''
-const spotifyRedirectUri = ''
+const spotifyClient= '' //Determined by Spotify their API requirements.
+const spotifySecret = '' ////Determined by Spotify their API requirements.
+const spotifyRedirectUri = '' //TODO: determine if this is required to be hardcoded.
 
 module.exports = {setlistFmKey,spotifyClient,spotifySecret,spotifyRedirectUri};
