445fbcdc453267cd7ca3d03e76fb9b7c7a392859
LRAB1 <lexbant@gmail.com>
2024-06-26 09:48:14 +0200

    comments added for clarity

--- keys.example.js
+++ keys.example.js
@@ -1,8 +1,8 @@
 //TODO: get the spotifyClient and Secret from the controller and not hardcode it.
 //for dev purposes this works but should be made into a filling variable.
 const setlistFmKey = ''
-const spotifyClient= '' //Determined by Spotify their API requirements.
-const spotifySecret = '' ////Determined by Spotify their API requirements.
+const spotifyClient= '' //TODO: deprecate by using it's variable in auth flow.
+const spotifySecret = '' //TODO: deprecate by using it's variable in auth flow.
 const spotifyRedirectUri = '' //Hardcoded for ease of use with the requirement from Spotify their API.
 
 module.exports = {setlistFmKey,spotifyClient,spotifySecret,spotifyRedirectUri};
