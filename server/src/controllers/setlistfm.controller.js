e92998f98a5aadf1193b181078f4ccf87b8368f3
LRAB1 <lexbant@gmail.com>
2024-09-30 15:41:30 +0200

    store it

--- server/src/controllers/setlistfm.controller.js
+++ server/src/controllers/setlistfm.controller.js
@@ -2,72 +2,20 @@ const https = require('follow-redirects').https;
 const fs = require('fs');
 const path = require('path');
 const { setlistFmKey } = require('../../../keys');
+const express = require('express');
+
+const app = express();
+
+const SETLISTFM_URL = 'https://setlist.fm/1.0/setlist/';
 
 //Create an array for debugging purposes, it shows the retrieved setlist.
 const retrievedSetlist = [];
 
-const searchSetlist = async (iD) => {
-  // Define the options for the Setlist.fm API request
-  const options = {
-    method: 'GET',
-    hostname: 'api.setlist.fm',
-    path: `/rest/1.0/setlist/${iD}`,
-    headers: {
-      accept: 'application/json',
-      'x-api-key': setlistFmKey,
-    },
-    maxRedirects: 20,
-  };
-
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, function (res) {
-      const chunks = [];
-
-      res.on('data', function (chunk) {
-        chunks.push(chunk);
-      });
-
-      res.on('end', function () {
-        const body = Buffer.concat(chunks);
-        const setlistData = body.toString();
-
-        try {
-          // Parse the setlistData to ensure it's valid JSON
-          const parsedSetlistData = JSON.parse(setlistData);
-
-          // Push the parsed setlist data to retrievedSetlist
-          retrievedSetlist.push(parsedSetlistData);
-
-          // Log the response for debugging
-          console.log(retrievedSetlist[0]);
-
-          // Export the retrievedSetlist to a JSON file
-          exportRetrievedSetlist();
-
-          resolve(setlistData);
-        } catch (error) {
-          reject(error);
-        }
-      });
-
-      res.on('error', function (error) {
-        reject(error);
-      });
-    });
-
-    req.end();
-  });
-};
-
-const exportRetrievedSetlist = () => {
-  const outputPath = path.join(__dirname, '../../data/raw-setlist.json');
-
+async function searchSetlist (req, res) {
   try {
-    // Write the retrievedSetlist to a JSON file
-    fs.writeFileSync(outputPath, JSON.stringify(retrievedSetlist, null, 2));
-    console.log(`Retrieved Setlist saved to ${outputPath}`);
-  } catch (error) {
-    console.error(`Error while saving retrieved Setlist: ${error.message}`);
+    return await fetch (`${SETLISTFM_URL}/${iD}`, {
+      
+    }})
   }
 };
 
