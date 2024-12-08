aa74b21bc990b410755f559935d80760a61d8d02
Lex Bant <lexbant@gmail.com>
2024-12-08 14:28:05 +0100

    cleaned up folder and file structuring to be more cohesive and in one place.

--- server/src/routers/setlist.router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const setlistfmController = require('../controllers/setlistfm.controller'); // Import your controller
-
-router.post('/searchSetlist', setlistfmController.searchSetlist); // Define the route
-
-module.exports = setlistFmRouter;
